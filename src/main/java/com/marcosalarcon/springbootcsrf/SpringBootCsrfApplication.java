99a7919e808972a9e296e3aca29e25a5ae046186
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2021-01-31 23:28:30 -0500

    first commit

---
+++ src/main/java/com/marcosalarcon/springbootcsrf/SpringBootCsrfApplication.java
@@ -0,0 +1,14 @@
+package com.marcosalarcon.springbootcsrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCsrfApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootCsrfApplication.class);
+    }
+
+}
---
+++ src/test/java/com/marcosalarcon/springbootcsrf/SpringBootCsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosalarcon.springbootcsrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCsrfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
